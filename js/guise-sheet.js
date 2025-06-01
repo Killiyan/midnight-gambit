079d5c1e05769cc402ad66df1488af823088dfaa
vaughn-killian <vaughn.killian@gmail.com>
2025-06-01 18:48:51 -0400

    Moves are now editable on Guise item

--- js/guise-sheet.js
+++ js/guise-sheet.js
@@ -10,6 +10,22 @@ export class GuiseSheet extends ItemSheet {
   getData(options) {
     const data = super.getData(options);
     data.system = this.item.system;
+
+    // Ensure array exists to prevent crash
+    data.system.moves ??= [];
+
     return data;
+  }
+
+
+  activateListeners(html) {
+  super.activateListeners(html);
+
+    html.find(".add-move").on("click", async (event) => {
+      event.preventDefault();
+      const moves = duplicate(this.item.system.moves || []);
+      moves.push({ name: "", description: "" });
+      await this.item.update({ "system.moves": moves });
+    });
   }
 }
